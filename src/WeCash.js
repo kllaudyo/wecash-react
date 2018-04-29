7f1a4ba31eb181b84899d4e4113e26c37327695f
Claudio Acioli <kllaudyo@gmail.com>
2018-04-29 11:51:58 -0300

    refactor: Limpa projeto

---
+++ src/WeCash.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class WeCash extends Component {
+    render() {
+        return (
+            <div>WeCash. desde 2012</div>
+        );
+    }
+}
+
+export default WeCash;
