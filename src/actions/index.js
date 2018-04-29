b5b1be1e2c0f9ac66cdeccea6a97c088fbd7a686
Claudio Acioli <kllaudyo@gmail.com>
2018-04-29 14:09:15 -0300

    feat: Adiciona actions categorias

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,32 @@
 import {
     FETCH_CONTAS_STARTED,
     FETCH_CONTAS_SUCCESS,
-    FETCH_CONTAS_FAILED
+    FETCH_CONTAS_FAILED,
+    FETCH_CATEGORIAS_SUCCESS,
+    FETCH_CATEGORIAS_STARTED,
+    FETCH_CATEGORIAS_FAILED
 } from "../constants";
 
 import * as api from '../api';
 
 export const
 
+    fetchContasStarted = () => ({
+        type: FETCH_CONTAS_STARTED
+    }),
+
+    fetchCategoriasStarted = () => ({
+        type: FETCH_CATEGORIAS_STARTED
+    }),
+
     fetchContasSuccess = contas => ({
         type : FETCH_CONTAS_SUCCESS,
         contas
+    }),
+
+    fetchCategoriasSuccess = categorias => ({
+        type: FETCH_CATEGORIAS_SUCCESS,
+        categorias
     }),
 
     fetchContasFailed = error => ({
@@ -18,8 +34,9 @@ export const
         error
     }),
 
-    fetchContasStarted = () => ({
-        type: FETCH_CONTAS_STARTED,
+    fetchCategoriasFailed = error => ({
+        type: FETCH_CATEGORIAS_FAILED,
+        error
     }),
 
     fetchContas = () => dispatch => {
@@ -27,5 +44,12 @@ export const
         api.getContas()
             .then( res => dispatch(fetchContasSuccess(res.data)))
             .catch( err => dispatch(fetchContasFailed( err.message)))
+    },
+
+    fetchCategorias = () => dispatch => {
+        dispatch(fetchCategoriasStarted());
+        api.getCategorias()
+            .then( res => dispatch(fetchCategoriasSuccess(res.data)))
+            .catch( err => dispatch(fetchCategoriasFailed(err.message)))
     }
 ;
