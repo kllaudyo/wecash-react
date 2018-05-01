b1c2f8d0d637f26512c692e13fe2d423bfac9ab8
Claudio Acioli <kllaudyo@gmail.com>
2018-05-01 14:22:19 -0300

    feat: Adiciona bootstrap

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import WeCash from './WeCash';
 import store from './store';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
